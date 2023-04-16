83515bb60eba8a5a8f79101e686d0e01a0509bb1
Guido Schweizer <guido.schweizer@posteo.de>
2023-04-16 15:25:03 +0200

    moved OO arround

--- exit_game.py
+++ exit_game.py
@@ -1,100 +1,102 @@
 class Room:
     def __init__(self, name, description):
-        self._name = name
-        self._description = description
-        self._neighbors = []
-        self._player = None
+        self.name = name
+        self.description = description
+        self.neighbors = []
 
     def add_neighbor(self, room):
-        self._neighbors.append(room)
-        room._neighbors.append(self)
+        self.neighbors.append(room)
+        room.neighbors.append(self)
 
-    def enter(self, player):
-        if self.is_unlocked():
-            self._player = player
-            player.current_room = self
-            print(f"You have entered {self._name}.")
-            print(self._description)
+    def enter(self):
+        print(f"You have entered {self.name}.")
+        print(self.description)
+        return self
 
     def move_to_next_room(self):
-        if not self.is_unlocked():
-            print(f"You need to solve the puzzle to enter {self._name}.")
-            return
-        if not self._neighbors:
+        if not self.neighbors:
             print("There are no neighboring rooms.")
-            return
-        print(f"From {self._name}, you can go to:")
-        for i, neighbor in enumerate(self._neighbors):
-            print(f"{i+1}. {neighbor._name}")
+            return self
+        print(f"From {self.name}, you can go to:")
+        for i, neighbor in enumerate(self.neighbors):
+            print(f"{i+1}. {neighbor.name}")
         while True:
-            choice = input(f"Enter your choice (1-{len(self._neighbors)}): ")
+            choice = input(f"Enter your choice (1-{len(self.neighbors)}): ")
             if choice.isnumeric():
                 choice = int(choice)
-                if 1 <= choice <= len(self._neighbors):
-                    next_room = self._neighbors[choice - 1]
-                    print(f"You are now entering {next_room._name}.")
-                    next_room.enter(self._player)
-                    break
+                if 1 <= choice <= len(self.neighbors):
+                    next_room = self.neighbors[choice - 1]
+                    print(f"You are now entering {next_room.name}.")
+                    return next_room.enter()
 
-    def is_unlocked(self):
-        return True
+    def is_locked(self):
+        return False
 
     def __str__(self):
-        return self._name
+        return self.name
 
 
-class LockedRoom(Room):
+class RiddleRoom(Room):
     def __init__(self, name, description, puzzle_question, puzzle_answer):
         super().__init__(name, description)
-        self._puzzle_question = puzzle_question
-        self._puzzle_answer = puzzle_answer
-        self._unlocked = False
+        self.puzzle_question = puzzle_question
+        self.puzzle_answer = puzzle_answer
+        self.locked = True
 
-    def is_unlocked(self):
-        return self._unlocked
+    def is_locked(self):
+        return self.locked
+    
+    def unlock(self):
+        print(self.puzzle_question)
+        answer = input().strip()
+        if answer.lower() == self.puzzle_answer.lower():
+            self.locked = False
+        else:
+            print(f"Incorrect answer. You cannot enter {self.name} until you solve the puzzle.")
+            return
 
-    def enter(self, player):
-        if not self.is_unlocked():
-            print(self._puzzle_question)
-            answer = input().strip()
-            if answer.lower() == self._puzzle_answer.lower():
-                self._unlocked = True
-            else:
-                print(f"Incorrect answer. You cannot enter {self._name} until you solve the puzzle.")
+    def enter(self):
+        if self.is_locked():
+            self.unlock()
+            if self.is_locked():
                 return
-        super().enter(player)
+        return super().enter()
 
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, room):
         self._name = name
-        self.current_room = None
+        self.current_room = room
 
     @property
     def name(self):
         return self._name
+    
+    def move_on(self):
+        moved_to = self.current_room.move_to_next_room()
+        if moved_to is not None:
+            self.current_room = moved_to
 
     def __str__(self):
         return self._name
 
 def all_rooms_unlocked(rooms):
     for room in rooms:
-        if not room.is_unlocked():
+        if room.is_locked():
             return False
-    print("Great, you're done!")
     return True
 
 def main():
     # Create rooms
     room1 = Room("Room 1", "This is room 1.")
-    room2 = LockedRoom("Room 2", "This is room 2.", "What is the capital of France?", "Paris")
-    room3 = LockedRoom("Room 3", "This is room 3.", "What is the square root of 16?", "4")
-    room4 = LockedRoom("Room 4", "This is room 4.", "What is the largest planet in our solar system?", "Jupiter")
-    room5 = LockedRoom("Room 5", "This is room 5.", "What is the largest continent by land area?", "Asia")
-    room6 = LockedRoom("Room 6", "This is room 6.", "What is the capital of Japan?", "Tokyo")
-    room7 = LockedRoom("Room 7", "This is room 7.", "What is the smallest country in the world by land area?", "Vatican City")
-    room8 = LockedRoom("Room 8", "This is room 8.", "What is the capital of Italy?", "Rome")
-    room9 = LockedRoom("Room 9", "This is room 9.", "What is the largest ocean on Earth?", "Pacific")
+    room2 = RiddleRoom("Room 2", "This is room 2.", "What is the capital of France?", "Paris")
+    room3 = RiddleRoom("Room 3", "This is room 3.", "What is the square root of 16?", "4")
+    room4 = RiddleRoom("Room 4", "This is room 4.", "What is the largest planet in our solar system?", "Jupiter")
+    room5 = RiddleRoom("Room 5", "This is room 5.", "What is the largest continent by land area?", "Asia")
+    room6 = RiddleRoom("Room 6", "This is room 6.", "What is the capital of Japan?", "Tokyo")
+    room7 = RiddleRoom("Room 7", "This is room 7.", "What is the smallest country in the world by land area?", "Vatican City")
+    room8 = RiddleRoom("Room 8", "This is room 8.", "What is the capital of Italy?", "Rome")
+    room9 = RiddleRoom("Room 9", "This is room 9.", "What is the largest ocean on Earth?", "Pacific")
 
     # Connect rooms
     room1.add_neighbor(room2)
@@ -109,12 +111,14 @@ def main():
     rooms = [room1,room2,room3,room4,room5,room6,room7,room8,room9]
 
     # Create player
-    player_name = input("Enter your name: ")
-    player = Player(player_name)
-    
-    room1.enter(player)
-    
+    playername = input("Enter your name: ")
+    player = Player(playername, room1)
+
+    print(f"Welcome {player.name}! You start in {room1}")
+        
     while not all_rooms_unlocked(rooms):
-        player.current_room.move_to_next_room()
-    
+        player.move_on()
+
+    print("Great, you're done!")
+
 main()
